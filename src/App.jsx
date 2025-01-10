0a474b30697a56157a14b1b1d35c1eccff98f1c5
zoooeKao <kaoyw0619@gmail.com>
2025-01-10 11:52:12 +0800

    chore: RWD

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {SubmitButton} from './components/Button';
 import {Category} from './components/Category';
 import {InStock} from './components/InStock';
@@ -15,6 +15,23 @@ import './index.css';
 export const App = () => {
   const [itemList, setItemList] = useState(/** @type {import('./context').FilterType} */ (items));
 
+  // 追蹤當前是否為手機模式
+  const [isMobile, setIsMobile] = useState(window.matchMedia('(max-width: 1024px)').matches);
+
+  // 當螢幕大小改變時更新 `isMobile`
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.matchMedia('(max-width: 1024px)').matches);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // 清除事件監聽器
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <Wrapper>
       <div className='w-4/5 lg:w-1/3 mb-3'>
@@ -23,21 +40,21 @@ export const App = () => {
         <InStock />
         <SubmitButton filterFn={(items) => setItemList(items)} />
       </div>
+      {/* 資料顯示部分 */}
       {itemList.length > 0 ? (
-        <div
-          className='w-4/5 lg:hidden'
-          data-desc='mobile'>
-          <ItemCard items={itemList} />
-        </div>
-      ) : (
-        <div>查無資料</div>
-      )}
-      {itemList.length > 0 ? (
-        <div
-          className='w-1/3 hidden lg:block'
-          data-desc='desktop'>
-          <ItemsTable items={itemList} />
-        </div>
+        isMobile ? (
+          <div
+            className='w-4/5 lg:hidden'
+            data-desc='mobile'>
+            <ItemCard items={itemList} />
+          </div>
+        ) : (
+          <div
+            className='w-1/3 hidden lg:block'
+            data-desc='desktop'>
+            <ItemsTable items={itemList} />
+          </div>
+        )
       ) : (
         <div>查無資料</div>
       )}
